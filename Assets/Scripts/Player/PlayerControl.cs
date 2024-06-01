344a80b985fe847e73e10bc22ff6dc851fbf121a
Daniel Chen <dchen110@usc.edu>
2024-06-01 13:41:56 -0700

    Implement crack

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -4,8 +4,10 @@ public class PlayerControl : MonoBehaviour
 {
     public readonly float NormalSpeed = 5f;
     public readonly float NormalJumpForce = 10f;
+    private const float FaintCountdownStart = 5f;
     public float speed; // may be changed by coffee spill zone
     public float jumpForce; // may be changed by coffee spill zone
+    private float faintCountdown = 0f; // used when dropped from crack
     private float horizontalInput;
     private bool isTryingToJump;
     private Rigidbody2D rb;
@@ -22,6 +24,12 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
+        if (faintCountdown > 0f)
+        {
+            faintCountdown -= Time.deltaTime;
+        }
+        SetPlayerSlowDown(faintCountdown > 0f);
+
         horizontalInput = Input.GetAxis("Horizontal");
         if (Input.GetKey(KeyCode.Space) && isGrounded && !climbLadder.isClimbing)
         {
@@ -35,19 +43,53 @@ public class PlayerControl : MonoBehaviour
         isTryingToJump = false;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (collision.gameObject.CompareTag("Floor"))
+        if (other.gameObject.CompareTag("Floor"))
         {
             isGrounded = true;
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionExit2D(Collision2D other)
     {
-        if (collision.gameObject.CompareTag("Floor"))
+        if (other.gameObject.CompareTag("Floor"))
         {
             isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("CoffeeSpill"))
+        {
+            SetPlayerSlowDown(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("CoffeeSpill"))
+        {
+            SetPlayerSlowDown(false);
+        }
+        else if (other.gameObject.CompareTag("Crack"))
+        {
+            faintCountdown = FaintCountdownStart; SetPlayerSlowDown(true);
+        }
+    }
+
+    private void SetPlayerSlowDown(bool slow)
+    {
+        if (slow)
+        {
+            speed = NormalSpeed / 3;
+            jumpForce = NormalJumpForce / 3;
+        }
+        else
+        {
+            speed = NormalSpeed;
+            jumpForce = NormalJumpForce;
         }
     }
 }
