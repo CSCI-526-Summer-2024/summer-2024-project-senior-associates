6f9b4e6f401bf1789f7ffd68d50b37bcf18e6c98
justinpshr <shr@usc.edu>
2024-06-03 19:19:32 -0700

    Added Clock and a new Finished Scene

---
+++ Assets/Scripts/SceneController/SceneController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    void Start()
+    {
+        SceneUpdate(0);    
+    }
+
+    public void SceneUpdate(int scene){
+        if(scene==0)
+        {
+            SceneManager.LoadScene("SampleScene",LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("SampleScene"));
+        }
+        else if(scene==1)
+        {
+            SceneManager.LoadScene("FinishScene",LoadSceneMode.Single);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("FinishScene"));
+        }
+    }
+}
