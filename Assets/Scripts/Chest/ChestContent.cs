396b3796fe366be764eeb6d0e138a0c30acf7201
Daniel Chen <dchen110@usc.edu>
2024-06-08 15:07:43 -0700

    Refactor manager request system

--- Assets/Scripts/Chest/ChestContent.cs
+++ Assets/Scripts/Chest/ChestContent.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
-    public GameObject itemPrefab;
+    public IngredientData ingredientData;
+    public int ingredientIndex;
+    private Ingredient ingredient;
+
+    void Start()
+    {
+        ingredient = ingredientData.allIngredients[ingredientIndex];
+    }
+
+    public Item GetItem()
+    {
+        return new Item
+        {
+            obj = Instantiate(ingredient.prefab),
+            type = Item.Type.Ingredient,
+            ingredients = new() { ingredient },
+        };
+    }
 }
