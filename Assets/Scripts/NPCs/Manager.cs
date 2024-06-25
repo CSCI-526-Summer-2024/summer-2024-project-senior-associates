4c8f6ecb6b30829314add90b9db4421d31056062
Daniel Chen <dchen110@usc.edu>
2024-06-24 17:30:56 -0700

    Weaken schmooze

--- Assets/Scripts/NPCs/Manager.cs
+++ Assets/Scripts/NPCs/Manager.cs
@@ -93,7 +93,7 @@ public class Manager : MonoBehaviour
 
     public void Schmooze()
     {
-        uiManager.UpdateScore(CalculateReward(request.reward, mood.Mood), transform.position);
+        uiManager.UpdateScore(CalculateReward(request.reward, mood.Mood) / 2, transform.position);
         FinishRequest();
     }
 
